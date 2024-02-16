2948aa475ea937962689f6fd288f897e6541216a
raul.souza <raul.souza@terceiro-sky.com.br>
2024-02-15 22:18:05 -0400

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,30 @@ export const App = () => {
     lat: 0,
     lgn: 0,
   });
-  console.log(
-    navigator.geolocation.getCurrentPosition((position) => {
-      setTe({ lat: position.coords.latitude, lgn: position.coords.longitude });
-    })
-  );
+  navigator.geolocation.getCurrentPosition((position) => {
+    setTe({ lat: position.coords.latitude, lgn: position.coords.longitude });
+  });
   return (
-    <a-scene
-      vr-mode-ui="enabled: false"
-      embedded
-      arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
-    >
-      <a-entity
-        gltf-model="./scenes/magnemite/scene.gltf"
-        rotation="0 180 0"
-        scale="0.15 0.15 0.15"
-        gps-entity-place={`longitude: -56.0287496; latitude: -15.6049534;`}
-        animation-mixer
-      />
-      <a-camera gps-camera rotation-reader></a-camera>
-    </a-scene>
+    <>
+      <h1>
+        lat: {te.lat} lgn: {te.lgn}
+      </h1>
+      <h1>longitude: -56.0287496; latitude: -15.6049534;</h1>
+      <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
+      >
+        <a-entity
+          gltf-model="./scenes/magnemite/scene.gltf"
+          rotation="0 180 0"
+          scale="0.15 0.15 0.15"
+          gps-entity-place={`longitude: -56.0287496; latitude: -15.6049534;`}
+          animation-mixer
+        />
+        <a-camera gps-camera rotation-reader></a-camera>
+      </a-scene>
+    </>
     // <Canvas camera={{ position: [0, 0, 100], fov: 15 }}>
     //   <ScrollControls pages={4} infinite>
     //     <Rig rotation={[0, 0, 0.15]}>
