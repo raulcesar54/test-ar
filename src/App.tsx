7b14a052db5c5fc6dc922570dd5de6e007301e24
raul.souza <raul.souza@terceiro-sky.com.br>
2024-02-15 22:30:08 -0400

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,15 @@ export const App = () => {
     setTe({ lat: position.coords.latitude, lgn: position.coords.longitude });
   });
   return (
-    <>
-      <h1>
-        lat: {te.lat} lgn: {te.lgn}
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="text-black font-bold">
+        lat: {te.lat} - lgn: {te.lgn}
       </h1>
       <h1>longitude: -56.0287496; latitude: -15.6049534;</h1>
       <a-scene
         vr-mode-ui="enabled: false"
         embedded
-        arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
+        arjs="sourceType: webcam; sourceWidth:900; sourceHeight:960; displayWidth: 900; displayHeight: 960; debugUIEnabled: false;"
       >
         <a-entity
           gltf-model="./scenes/magnemite/scene.gltf"
@@ -29,7 +29,7 @@ export const App = () => {
         />
         <a-camera gps-camera rotation-reader></a-camera>
       </a-scene>
-    </>
+    </div>
     // <Canvas camera={{ position: [0, 0, 100], fov: 15 }}>
     //   <ScrollControls pages={4} infinite>
     //     <Rig rotation={[0, 0, 0.15]}>
